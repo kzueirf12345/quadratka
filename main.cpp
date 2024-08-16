ef7e169639ef11f75b280f4c6b525f81343ed6d4
Vladimir <jaba0192837465@gmail.com>
2024-08-16 18:16:13 +0300

    split the program into input, calculation and output

--- main.cpp
+++ main.cpp
@@ -4,62 +4,96 @@
 #define EPS 10e-7
 #define NEPS -10e-7
 
-int gcd(int a, int b);
-void print_one_answer(double answer);
-void print_two_answer(double answer1, double answer2);
+enum OutputState {
+    ZERO_SOLUTIONS,
+    ONE_SOLUTIONS,
+    TWO_SOLUTIONS,
+    INF_SOLUTIONS,
+    NOT_REAL_SOLUTIONS
+};
+void input(int* a, int* b, int* c);
+void calculate(int a, int b, int c, double* answer1, double* answer2, enum OutputState* state);
+void print(double answer1, double answer2, enum OutputState state);
 
 int main() {
     int a, b, c;
-    printf("Input first coef: ");
-    scanf("%d", &a);
-    printf("Input second coef: ");
-    scanf("%d", &b);
-    printf("Input third coef: ");
-    scanf("%d", &c);
+    input(&a, &b, &c);
 
-    printf("answer: ");
+    double answer1, answer2;
+    enum OutputState state;
+
+    calculate(a, b, c, &answer1, &answer2, &state);
+    
+    print(answer1, answer2, state);
+
+    return 0;
+}
+
+void input(int* a, int* b, int* c) {
+    printf("Input first coef: ");
+    scanf("%d", a);
+    printf("Input second coef: ");
+    scanf("%d", b);
+    printf("Input third coef: ");
+    scanf("%d", c);
+}
+
+void calculate(int a, int b, int c, double* answer1, double* answer2, enum OutputState* state) {
     if (a == 0) {
         if (b == 0) {
-            printf(c == 0 ? "x - any number\n" : "no solutions\n");
+            if (c == 0) {
+                *state = INF_SOLUTIONS;
+            } else {
+                *state = ZERO_SOLUTIONS;
+            }
         } else {
-            print_one_answer(-c * 1. / b);
+            *state = ONE_SOLUTIONS;
+            *answer1 = -c * 1. / b;
         }
     } else {
         int discriminant = b * b - 4 * a * c;
         if (discriminant < 0) {
-            printf("solutions is not real\n");
+            *state = NOT_REAL_SOLUTIONS;
         } else if (discriminant == 0) {
-            print_one_answer(-b * 1. / 2 * a);
+            *state = ONE_SOLUTIONS;
+            *answer1 = -b * 1. / 2 * a;
         } else {
-            double discriminant_double = discriminant;
-            double numerator1 = -b - sqrt(discriminant_double),
-                   numerator2 = -b + sqrt(discriminant_double);
+            *state = TWO_SOLUTIONS;
+            double numerator1 = -b - sqrt((double)discriminant),
+                   numerator2 = -b + sqrt((double)discriminant);
             int denominator = 2 * a;
-            print_two_answer(numerator1 / denominator, numerator2 / denominator);
+
+            *answer1 = numerator1 / denominator;
+            *answer2 = numerator2 / denominator;
         }
     }
-    return 0;
 }
 
-int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
-
-void print_one_answer(double answer) {
-    if (abs(answer - (int)answer) < EPS) {
-        printf("x = %d\n", (int)answer);
-    } else {
-        printf("x = %f\n", answer);
-    }
-}
-
-void print_two_answer(double answer1, double answer2) {
-    if (abs(answer1 - (int)answer1) < EPS) {
-        printf("x = %d, ", (int)answer1);
-    } else {
-        printf("x = %f, ", answer1);
-    }
-    if (abs(answer2 - (int)answer2) < EPS) {
-        printf("%d\n", (int)answer2);
-    } else {
-        printf("%f\n", answer2);
+void print(double answer1, double answer2, enum OutputState state) {
+    printf("Answer: ");
+    switch (state) {
+        case ZERO_SOLUTIONS:
+            printf("zero solutions\n");
+            break;
+        case ONE_SOLUTIONS: {
+            printf("x = %g\n", answer1);
+            break;
+        }
+        case TWO_SOLUTIONS: {
+            printf("x = %g, %g\n", answer1, answer2);
+            break;
+        }
+        case INF_SOLUTIONS: {
+            printf("x - any number\n");
+            break;
+        }
+        case NOT_REAL_SOLUTIONS: {
+            printf("no real solutions\n");
+            break;
+        }
+        default: {
+            fprintf(stderr, "Unknown error\n");
+            break;
+        }
     }
 }
