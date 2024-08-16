1447d9e2d9db484a012691c4688f4c677d2e0a7c
Vladimir <jaba0192837465@gmail.com>
2024-08-17 01:06:12 +0300

    from int to double and fix -0 in answer

--- main.cpp
+++ main.cpp
@@ -11,61 +11,69 @@ enum OutputState {
     INF_SOLUTIONS,
     NOT_REAL_SOLUTIONS
 };
-void input(int* a, int* b, int* c);
-void calculate(int a, int b, int c, double* answer1, double* answer2, enum OutputState* state);
+void input(double* a, double* b, double* c);
+void calculate(double a, double b, double c, double* answer1, double* answer2,
+               enum OutputState* state);
 void print(double answer1, double answer2, enum OutputState state);
 
 int main() {
-    int a, b, c;
+    double a, b, c;
     input(&a, &b, &c);
 
     double answer1, answer2;
     enum OutputState state;
 
     calculate(a, b, c, &answer1, &answer2, &state);
-    
+
     print(answer1, answer2, state);
 
     return 0;
 }
 
-void input(int* a, int* b, int* c) {
+void input(double* a, double* b, double* c) {
     printf("Input first coef: ");
-    scanf("%d", a);
+    scanf("%lf", a);
     printf("Input second coef: ");
-    scanf("%d", b);
+    scanf("%lf", b);
     printf("Input third coef: ");
-    scanf("%d", c);
+    scanf("%lf", c);
 }
 
-void calculate(int a, int b, int c, double* answer1, double* answer2, enum OutputState* state) {
-    if (a == 0) {
-        if (b == 0) {
-            if (c == 0) {
+void calculate(double a, double b, double c, double* answer1, double* answer2,
+               enum OutputState* state) {
+    if (abs(a) < EPS) {
+        if (abs(b) < EPS) {
+            if (abs(c) < EPS) {
                 *state = INF_SOLUTIONS;
             } else {
                 *state = ZERO_SOLUTIONS;
             }
         } else {
             *state = ONE_SOLUTIONS;
-            *answer1 = -c * 1. / b;
+            *answer1 = -c / b;
         }
     } else {
-        int discriminant = b * b - 4 * a * c;
+        double discriminant = b * b - 4 * a * c;
         if (discriminant < 0) {
             *state = NOT_REAL_SOLUTIONS;
-        } else if (discriminant == 0) {
+        } else if (abs(discriminant) < EPS) {
             *state = ONE_SOLUTIONS;
-            *answer1 = -b * 1. / (2 * a);
+            *answer1 = -b / (2 * a);
         } else {
             *state = TWO_SOLUTIONS;
-            double numerator1 = -b - sqrt((double)discriminant),
-                   numerator2 = -b + sqrt((double)discriminant);
-            int denominator = 2 * a;
+            double numerator1 = -b - sqrt(discriminant), numerator2 = -b + sqrt(discriminant);
+            double denominator = 2 * a;
 
             *answer1 = numerator1 / denominator;
             *answer2 = numerator2 / denominator;
         }
+    }
+
+    if (abs(*answer1) < EPS) {
+        *answer1 = 0;
+    }
+    if (abs(*answer2) < EPS) {
+        *answer2 = 0;
     }
 }
 
