5cd45b645709b13c3889b5f7ee187eaf35b5ff15
Vladimir <jaba0192837465@gmail.com>
2024-08-17 14:20:48 +0300

    linear and quadratic calculate

--- main.cpp
+++ main.cpp
@@ -10,9 +10,18 @@ enum CountSolutions {
     INF_SOLUTIONS,
     NOT_REAL_SOLUTIONS
 };
+
+
 void input(double* const a, double* const b, double* const c);
+
+void linear_calculate(const double b, const double c, double* const answer1,
+                      CountSolutions* const count_solutions);
+void quadratic_calculate(const double a, const double b, const double c, double* const answer1,
+                         double* const answer2, CountSolutions* const count_solutions);
 void calculate(const double a, const double b, const double c, double* const answer1,
                double* const answer2, CountSolutions* const count_solutions);
+
+double fix_double_zero(double num);
 void print(const double answer1, const double answer2, const CountSolutions count_solutions);
 
 int main() {
@@ -29,6 +38,13 @@ int main() {
     return 0;
 }
 
+double fix_double_zero(double num) {
+    if (abs(num) < EPS) {
+        return 0;
+    }
+    return num;
+}
+
 void input(double* const a, double* const b, double* const c) {
     printf("Input first coef: ");
     scanf("%lf", a);
@@ -38,38 +54,41 @@ void input(double* const a, double* const b, double* const c) {
     scanf("%lf", c);
 }
 
+void linear_calculate(const double b, const double c, double* const answer1,
+                      CountSolutions* const count_solutions) {
+    if (abs(b) < EPS) {
+        if (abs(c) < EPS) {
+            *count_solutions = INF_SOLUTIONS;
+        } else {
+            *count_solutions = ZERO_SOLUTIONS;
+        }
+    } else {
+        *count_solutions = ONE_SOLUTIONS;
+        *answer1 = -c / b;
+    }
+}
+
+void quadratic_calculate(const double a, const double b, const double c, double* const answer1,
+                         double* const answer2, CountSolutions* const count_solutions) {
+    double discriminant = b * b - 4 * a * c;
+    if (discriminant < 0) {
+        *count_solutions = NOT_REAL_SOLUTIONS;
+    } else if (abs(discriminant) < EPS) {
+        *count_solutions = ONE_SOLUTIONS;
+        *answer1 = -b / (2 * a);
+    } else {
+        *count_solutions = TWO_SOLUTIONS;
+        *answer1 = (-b - sqrt(discriminant)) / (2 * a);
+        *answer2 = (-b + sqrt(discriminant)) / (2 * a);
+    }
+}
+
 void calculate(const double a, const double b, const double c, double* const answer1,
                double* const answer2, CountSolutions* const count_solutions) {
     if (abs(a) < EPS) {
-        if (abs(b) < EPS) {
-            if (abs(c) < EPS) {
-                *count_solutions = INF_SOLUTIONS;
-            } else {
-                *count_solutions = ZERO_SOLUTIONS;
-            }
-        } else {
-            *count_solutions = ONE_SOLUTIONS;
-            *answer1 = -c / b;
-        }
+        linear_calculate(b, c, answer1, count_solutions);
     } else {
-        double discriminant = b * b - 4 * a * c;
-        if (discriminant < 0) {
-            *count_solutions = NOT_REAL_SOLUTIONS;
-        } else if (abs(discriminant) < EPS) {
-            *count_solutions = ONE_SOLUTIONS;
-            *answer1 = -b / (2 * a);
-        } else {
-            *count_solutions = TWO_SOLUTIONS;
-            *answer1 = (-b - sqrt(discriminant)) / (2 * a);
-            *answer2 = (-b + sqrt(discriminant)) / (2 * a);
-        }
-    }
-
-    if (abs(*answer1) < EPS) {
-        *answer1 = 0;
-    }
-    if (abs(*answer2) < EPS) {
-        *answer2 = 0;
+        quadratic_calculate(a, b, c, answer1, answer2, count_solutions);
     }
 }
 
@@ -80,11 +99,11 @@ void print(const double answer1, const double answer2, const CountSolutions coun
             printf("zero solutions\n");
             break;
         case ONE_SOLUTIONS: {
-            printf("x = %g\n", answer1);
+            printf("x = %g\n", fix_double_zero(answer1));
             break;
         }
         case TWO_SOLUTIONS: {
-            printf("x = %g, %g\n", answer1, answer2);
+            printf("x = %g, %g\n", fix_double_zero(answer1), fix_double_zero(answer2));
             break;
         }
         case INF_SOLUTIONS: {
