057f3b1e40a5bad904cbe6d2054ce897c68d836c
Vladimir <jaba0192837465@gmail.com>
2024-08-20 16:24:41 +0300

    refactor input, return instead of ptr, is_zero and cycle input

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-#include "assert.h"
-#include "ctype.h"
-#include "math.h"
-#include "stdio.h"
-#include "string.h"
+#include <assert.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
-#define EPS 10e-7
+#define EPS 1e-7
+
+enum ReturnCode
+{
+    RETURN_FAILURE,
+    RETURN_SUCCESS
+};
 
 enum CountSolutions
 {
@@ -29,151 +35,148 @@ struct Answer
     CountSolutions count_solutions;
 };
 
-bool is_num(const char* const string);
-void input(Coefs* coefs);
-
-void linear_calculate(const Coefs* const coefs, Answer* const answer);
-void quadratic_calculate(const Coefs* const coefs, Answer* const answer);
-void calculate(const Coefs* const coefs, Answer* const answer);
-
+bool is_zero(double num);
 double fix_double_zero(double num);
-void print(const Answer* const answer);
+
+void flush(void);
+
+ReturnCode scan_double(double* const num);
+void input_coef(double* const num, const char* const message);
+void input(Coefs* const coefs);
+
+Answer linear_calculate(const Coefs coefs);
+Answer quadratic_calculate(const Coefs coefs);
+Answer calculate(const Coefs coefs);
+
+void print(const Answer answer);
 
 int main()
 {
     Coefs coefs = {0, 0, 0};
     input(&coefs);
 
-    Answer answer = {0, 0, ZERO_SOLUTIONS};
-    calculate(&coefs, &answer);
-
-    print(&answer);
+    const Answer answer = calculate(coefs);
+    print(answer);
 
     return 0;
 }
 
-bool is_num(const char* const string)
-{
-    bool is_already_dot = false;
-    size_t size = strlen(string);
-    if (*string == '+' || *string == '-' || isdigit(*string))
-    {
-        for (size_t i = 1; i < size; ++i)
-        {
-            if (!isdigit(string[i]))
-            {
-                if (string[i] == '.')
-                {
-                    if (is_already_dot)
-                    {
-                        return false;
-                    }
-                    is_already_dot = true;
-                } else
-                {
-                    return false;
-                }
-            }
-        }
-    } else
-    {
-        return false;
-    }
-    return true;
-}
+bool is_zero(double num) { return abs(num) < EPS; }
 
-void input(Coefs* coefs)
-{
-    char buffer[100];
-
-    printf("Input first coef: ");
-    scanf("%s", buffer);
-    assert(is_num(buffer) && "incorrect input\n");
-    coefs->a = atof(buffer);
-
-    printf("Input second coef: ");
-    scanf("%s", buffer);
-    assert(is_num(buffer) && "incorrect input\n");
-    coefs->b = atof(buffer);
-
-    printf("Input third coef: ");
-    scanf("%s", buffer);
-    assert(is_num(buffer) && "incorrect input\n");
-    coefs->c = atof(buffer);
-}
-
-void linear_calculate(const Coefs* const coefs, Answer* const answer) 
-{
-    if (abs(coefs->b) < EPS) 
-    {
-        if (abs(coefs->c) < EPS) 
-        {
-            answer->count_solutions = INF_SOLUTIONS;
-        } else 
-        {
-            answer->count_solutions = ZERO_SOLUTIONS;
-        }
-    } else 
-    {
-        answer->root1 = -coefs->c / coefs->b;
-        answer->count_solutions = ONE_SOLUTIONS;
-    }
-}
-
-void quadratic_calculate(const Coefs* const coefs, Answer* const answer)
-{
-    double discriminant = coefs->b * coefs->b - 4 * coefs->a * coefs->c;
-    if (discriminant < 0) 
-    {
-        answer->count_solutions = NOT_REAL_SOLUTIONS;
-    } else if (abs(discriminant) < EPS) 
-    {
-        answer->count_solutions = ONE_SOLUTIONS;
-        answer->root1 = -coefs->b / (2 * coefs->a);
-    } else 
-    {
-        answer->root1 = (-coefs->b - sqrt(discriminant)) / (2 * coefs->a);
-        answer->root2 = (-coefs->b + sqrt(discriminant)) / (2 * coefs->a);
-        answer->count_solutions = TWO_SOLUTIONS;
-    }
-}
-
-void calculate(const Coefs* const coefs, Answer* const answer) 
-{
-    if (abs(coefs->a) < EPS) 
-    {
-        linear_calculate(coefs, answer);
-    } else 
-    {
-        quadratic_calculate(coefs, answer);
-    }
-}
-
-double fix_double_zero(double num) 
-{
-    if (abs(num) < EPS) 
+double fix_double_zero(double num) {
+    if (is_zero(abs(num))) 
     {
         return 0;
     }
     return num;
 }
 
-void print(const Answer* const answer) 
+
+void flush(void)
+{
+    for (int symbol = getchar(); symbol != '\n' && symbol != EOF; symbol = getchar());
+}
+
+ReturnCode scan_double(double* const num) 
+{
+    int correct_scan_count = scanf("%lg", num);
+    int next_symbol = getchar();
+
+    return (ReturnCode)(correct_scan_count != 0 && next_symbol == (int)'\n');
+}
+
+void input_coef(double* const num, const char* const message)
+{
+    printf("%s", message);
+    while (scan_double(num) == RETURN_FAILURE)
+    {
+        printf("Incorrect input, try again!\n");
+        printf("%s", message);
+        flush();
+    }
+}
+
+void input(Coefs* const coefs) {
+    input_coef(&coefs->a, "Input first coef: ");
+    input_coef(&coefs->b, "Input second coef: ");
+    input_coef(&coefs->c, "Input third coef: ");
+}
+
+
+Answer linear_calculate(const Coefs coefs)
+{
+    Answer answer = {0, 0, INF_SOLUTIONS};
+    if (is_zero(abs(coefs.b))) 
+    {
+        if (is_zero(abs(coefs.c))) 
+        {
+            answer.count_solutions = INF_SOLUTIONS;
+        }
+        else
+        {
+            answer.count_solutions = ZERO_SOLUTIONS;
+        }
+    }
+    else
+    {
+        answer.root1 = -coefs.c / coefs.b;
+        answer.count_solutions = ONE_SOLUTIONS;
+    }
+    return answer;
+}
+
+Answer quadratic_calculate(const Coefs coefs)
+{
+    Answer answer = {0, 0, INF_SOLUTIONS};
+    double discriminant = coefs.b * coefs.b - 4 * coefs.a * coefs.c;
+    if (discriminant < 0) 
+    {
+        answer.count_solutions = NOT_REAL_SOLUTIONS;
+    }
+    else if (is_zero(abs(discriminant))) 
+    {
+        answer.count_solutions = ONE_SOLUTIONS;
+        answer.root1 = -coefs.b / (2 * coefs.a);
+    }
+    else
+    {
+        answer.root1 = (-coefs.b - sqrt(discriminant)) / (2 * coefs.a);
+        answer.root2 = (-coefs.b + sqrt(discriminant)) / (2 * coefs.a);
+        answer.count_solutions = TWO_SOLUTIONS;
+    }
+    return answer;
+}
+
+Answer calculate(const Coefs coefs)
+{
+    if (is_zero(abs(coefs.a))) 
+    {
+        return linear_calculate(coefs);
+    }
+    else 
+    {
+        return quadratic_calculate(coefs);
+    }
+}
+
+void print(const Answer answer)
 {
     printf("Answer: ");
-    switch (answer->count_solutions) 
+    switch (answer.count_solutions) 
     {
         case ZERO_SOLUTIONS:
+        {
             printf("zero solutions\n");
             break;
+        }
         case ONE_SOLUTIONS: 
         {
-            printf("x = %g\n", fix_double_zero(answer->root1));
+            printf("x = %g\n", fix_double_zero(answer.root1));
             break;
         }
         case TWO_SOLUTIONS: 
         {
-            printf("x = %g, %g\n", fix_double_zero(answer->root1), fix_double_zero(answer->root2));
+            printf("x = %g, %g\n", fix_double_zero(answer.root1), fix_double_zero(answer.root2));
             break;
         }
         case INF_SOLUTIONS: 
@@ -188,7 +191,7 @@ void print(const Answer* const answer)
         }
         default: 
         {
-            assert("Unknown error\n");
+            assert(0 && "Unknown error\n");
             break;
         }
     }
